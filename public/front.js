938b5c5bb711664466724aaff84629710fadcd43
DavidKniotek <dawid.kniotek@gmail.com>
2023-01-26 18:28:57 +0100

    front & calc js files has been created

---
+++ public/front.js
@@ -0,0 +1,22 @@
+const numberAInput = document.querySelector('#number-a');
+const numberBInput = document.querySelector('#number-b');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const numberA = Number(numberAInput.value);
+    const numberB = Number(numberBInput.value);
+
+    const result = fetch('/calc/check', {
+        method: 'POST',
+        body: JSON.stringify({
+            numberA,
+            numberB,
+        }),
+        headers: {
+            'Content-type': 'application/json',
+        },
+    });
+    const data = await result.json();
+});
